04747c492ec2c7fa6fbca280fd08d9897df3645c
= <=>
2024-09-19 20:09:05 -0300

    Change index

--- core/views.py
+++ core/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
-    template_name = 'core/home.html'
+    template_name = 'core/index.html'
